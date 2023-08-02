3805202611acaf674118b3cc0f6e0eca31c48e7e
EC2 Default User <ec2-user@ip-172-31-9-136.ec2.internal>
2023-08-02 21:13:41 +0000

    add convert module to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(name='xenaConvert',
       version='0.1',
       description='Python Utilities',
-      author='Jing Zhu',
-      author_email='jzhu@soe.ucsc.edu',
       url='https://github.com/ucscXena/xenaConvert',
+      py_modules = ['convert'],
       install_requires=[
         'pandas',
         'scanpy',
