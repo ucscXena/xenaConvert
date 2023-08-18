bad0c0850d192480b59144ffd910d9ab17ed70a2
jzhu <jzhu@jzhus-MacBook-Pro.local>
2023-08-18 14:39:31 -0700

    add     adata = basic_analysis(adatai) option to h5adToXena

--- xenaConvert.py
+++ xenaConvert.py
@@ -329,11 +329,13 @@ def loomToXena(matrixFname, outputpath, studyName, transpose = True):
     adata = sc.read(matrixFname, first_column_names=True)
     adataToXena(adata, outputpath, studyName, transpose = transpose)
 
-def h5adToXena(h5adFname, outputpath, studyName):
+def h5adToXena(h5adFname, outputpath, studyName, basic_analysis = False):
     """
     Given a h5ad file, write dataset to a dataset directory under path.
     """
     adata = sc.read_h5ad(h5adFname)
+    if (basic_analysis):
+        adata = basic_analysis(adata)
     adataToXena(adata, outputpath, studyName)
 
 def basic_analysis(adata, normalization = True):
